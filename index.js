e9b6630ee49c56a7076fbc3b24295ef8401fe5e0
NdeyeAmie <tndyeamie456@gmail.com>
2025-05-10 21:47:03 +0000

    installation package

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db")
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  });
