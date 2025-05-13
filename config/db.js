519cc9418c0a22f4d3621663b911336c1e79bc82
Saliou <salljunior439@gmail.com>
2025-05-13 13:36:03 +0000

    creation model

--- config/db.js
+++ config/db.js
@@ -1,15 +1,6 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const connectDB = async () => {
-    
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("✅ Connexion à MongoDB réussie");
-  } catch (error) {
-    console.error(" Erreur de connexion à MongoDB :", error.message);
-    process.exit(1); 
-  }
-};
+
 
 module.exports = connectDB;
---
+++ model/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    nom: {
+        type: String,
+        required: true,
+    },
+    prenom: {
+        type: String,
+        required: true,
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model("User", userSchema);
