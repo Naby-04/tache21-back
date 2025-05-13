747f02843d7627b9b025e8b2369be40cb88726d6
binta dia <bintadia269@gmail.com>
2025-05-13 17:53:43 +0200

    rappoer

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db")
-
+const mongoose = require("mongoose");
 
 dotenv.config();
 const app = express();
@@ -10,9 +10,12 @@ app.use(express.json());
 app.use(cors());
 
 
-connectDB()
+mongoose.connect("mongodb+srv://back:back1234@cluster0.bbasbd8.mongodb.net/back?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   });
+  app.get("/", (req, res) => {
+    res.send("hello saliou and baba nafina ...");
+  })
