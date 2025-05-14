33c8c41a939f657e5ed65c2ff0c6ea2d60011dbd
NdeyeAmie <tndyeamie456@gmail.com>
2025-05-14 17:01:27 +0000

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-// const connectDB = require("./config/db")
-const mongoose = require("mongoose");
+const connectDB = require("./config/db.js");
+
 
 dotenv.config();
 const app = express();
@@ -10,12 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 
-mongoose.connect("mongodb+srv://back:back1234@cluster0.bbasbd8.mongodb.net/back?retryWrites=true&w=majority&appName=Cluster0")
+connectDB()
 .then(() => {
-    app.listen(8080, () => {
-      console.log(`Server running on port 8080}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
     });
   });
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
-  })
