1c900b9b1a137b7b6937d641899d8583b9d3a35e
NdeyeAmie <tndyeamie456@gmail.com>
2025-05-15 15:05:30 +0000

    api-users

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
-
+const {errorHandler} = require("./middleware/errorMiddleware.js")
+const usersRoutes = require("./routes/usersRoutes.js")
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
+// Routes
+app.use("/api/users", usersRoutes);
+
+// Gestion des erreurs
+app.use(errorHandler);
 
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT, () => {
+      console.log("PORT from .env:", process.env.PORT);
+
       console.log(`Server running on port ${process.env.PORT}`);
     });
   });
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/userModel");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      if (!req.user) {
+        return res.status(401).json({ message: "Utilisateur introuvable" });
+      }
+
+      next();
+    } catch (err) {
+      console.error("Erreur dans protect :", err);
+      return res.status(401).json({ message: "Token invalide ou expiré" });
+    }
+  } else {
+    return res.status(401).json({ message: "Non autorisé, pas de token" });
+  }
+};
+
+
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).json({ message: "Accès refusé : admin uniquement" });
+  }
+};
+
+
+
+
+module.exports = { protect ,admin };
