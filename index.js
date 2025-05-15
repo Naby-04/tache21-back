2ecaf217ba0adc47cabbf5e87e5f9ec56cce1e92
NdeyeAmie <tndyeamie456@gmail.com>
2025-05-15 15:16:52 +0000

    merger la branch back-j

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
-
+const {errorHandler} = require("./middleware/errorMiddleware.js")
+const usersRoutes = require("./routes/usersRoutes.js")
+const rapportRoutes = require("./routes/Rapport");
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-const rapportRoutes = require("./routes/Rapport");
+// Gestion des erreurs
+app.use(errorHandler);
+
+// Routes
+app.use("/api/users", usersRoutes);
 app.use("/rapport", rapportRoutes);
 
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT, () => {
+      console.log("PORT from .env:", process.env.PORT);
+
       console.log(`Server running on port ${process.env.PORT}`);
     });
   });
