bd2a8bfcca3e414142a0cd552817eb1574c1a9bd
NdeyeAmie <tndyeamie456@gmail.com>
2025-05-23 15:20:27 +0000

    merger branch  page-Accueil

--- controllers/rapportController.js
+++ controllers/rapportController.js
@@ -68,7 +68,7 @@ const getAllRapports = async (req, res) => {
     // const rapports = await Rapport.find({}).sort({ createdAt: -1 });
     const rapports = await Rapport.find({})
       .sort({ createdAt: -1 })
-      .populate('user', 'prenom');
+      .populate('userId', 'prenom');
     return res.status(200).json(rapports);
   } catch (error) {
     return res.status(500).json({ message: "Impossible de récupérer les rapports" });
